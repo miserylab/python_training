3d998ad3a37be0975147eff743309d68ebad16c1
Olga Demina <miserylab.r6s@gmail.com>
2022-04-18 16:41:10 +0300

    Добавлен автор заработал кейс в хроме(версия драйвера не поддерживала текущую версию браузера)

--- test_add_group2.py
+++ test_add_group2.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+__author__ = 'miserylab'
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
