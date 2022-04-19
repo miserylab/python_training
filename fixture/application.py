008d3982e45f8bca4f99c001611e6cfd3ee9e674
Olga Demina <miserylab.r6s@gmail.com>
2022-04-19 15:10:35 +0300

    Выделен вспомогательный класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -3,6 +3,7 @@ __author__ = 'miserylab'
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from python_training.fixture.session import SessionHelper
+from python_training.fixture.group import GroupHelper
 
 
 class Application:
@@ -10,35 +11,7 @@ class Application:
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element(by=By.LINK_TEXT, value="group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element(by=By.NAME, value="new").click()
-        # fill group form
-        wd.find_element(by=By.NAME, value="group_name").click()
-        wd.find_element(by=By.NAME, value="group_name").clear()
-        wd.find_element(by=By.NAME, value="group_name").send_keys(group.name)
-        wd.find_element(by=By.NAME, value="group_header").click()
-        wd.find_element(by=By.NAME, value="group_header").clear()
-        wd.find_element(by=By.NAME, value="group_header").send_keys(group.header)
-        wd.find_element(by=By.NAME, value="group_footer").click()
-        wd.find_element(by=By.NAME, value="group_footer").clear()
-        wd.find_element(by=By.NAME, value="group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element(by=By.NAME, value="submit").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element(by=By.LINK_TEXT, value="groups").click()
-
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
